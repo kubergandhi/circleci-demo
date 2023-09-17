802c450455583653a297a89e1a4ae24b1fb4f2d1
kubergandhi <138006454+kubergandhi@users.noreply.github.com>
2023-09-17 17:47:49 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("wassup world from srcmake")
+        print("wassup world from kuber")
 
 if __name__ == '__main__':
         SayHello()
